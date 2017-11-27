b2c6dd856f136ba37ab0ea15a9cfb07dde3079d0
railgunxin <1307881527@qq.com>
2017-11-27 16:30:55 +0800

    小相册

--- Album/controller/router.js
+++ Album/controller/router.js
@@ -1,39 +1,28 @@
+// 引入首页html模板
 var file = require('../models/file.js');
-// 首页
-exports.showIndex = function(req,res,next){
-    // 不能这么写：因为是异步的这个数组还没有return，就会跳过执行下一条语句
-    // res.render('index',{
-    //     "albums" : file.getAllAlbums()
-    // });
 
-    // Node.js的编程思维，就是所有的东西都是异步的
-    //所以，内层函数，不是return回来东西，而是调用高层函数提供的
-    //回调函数，把数据当做回调函数的参数来使用。
+// 导出渲染后的首页
+exports.showIndex = function(req,res,next){
+    // res.render('index',{
+    //     'albums':file.getAllAlbums()
+    // })
     file.getAllAlbums(function(err,allAlbums,next){
-        if (err) {
-            next();//交给下面的中间件
-            return;
-        }
+        if(err) next();
+        
         res.render('index',{
-            'albums': allAlbums
-        });
+            'albums':allAlbums
+        })
     })
 }
 
 // 相册页
-exports.showAlbum = function(req,res){
-    // 遍历相册中的所有图片
-    // res.send('相册'+ req.params.albumName);
+exports.showAlbum = function (req,res) {
     var albumName = req.params.albumName;
-    //具体业务交给model
     file.getAllImagesByAlbumName(albumName,function(err,imagesArray){
-        if (err) {
-            res.render('err');
-            return;
-        }
+        if (err) res.render('err');
         res.render('album',{
             'albumname':albumName,
             'images':imagesArray
         })
-    });
+    })
 }
--- Album/models/file.js
+++ Album/models/file.js
@@ -1,45 +1,69 @@
+// 读取文件就要用到fs模块
 var fs = require('fs');
 
-exports.getAllAlbums = function(callback){
+// 导出一个存放所有albums文件夹的数组
+
+exports.getAllAlbums = function (callback) {
+    //readdir读取xx文件夹下的文件目录（第一个参数err）
+    //（第二个参数files 是文件目录中不包括 '.' 和 '..' 的文件名的数组。）
     fs.readdir('./uploads',function(err,files){
-        if (err) callback('没找到文件夹',null);
+        // 读取一个文件的步骤就是：第一步先检查是否存在`./uploads`，如果不存在给一个提示
+        // 以后遇到fs模块中读取文件方法首先判断是否存在该文件，然后再判断接下来怎么做~
+        if(err) calback('没有找到文件夹',null);
+           
+        // 声明一个数组存放相册目录
         var allAlbums = [];
+        // 迭代循环‘./uploads’的文件目录添加到声明的数组
+        // 我们的目的是读取files然后把files中所有数据值添加到allAlbums中
+
+        // 按照正常逻辑应该这么做。。。
+        // for(var i =0; i<files.length; i++){
+        //      fs.stat() 检查一个文件是否存在。fs.stat(path,callback)
+        //      callback(err,stats) stats 是一个 fs.Stats 对象。stats.isFile() stats.isDirectory()
+        //      因为Node是异步的，第一个文件还没读完就执行第二次循环了
+        //     fs.stat('./uploads'+files[i],function(err,stats){
+        //         if(err) callback('找不到文件~'+ files[i],null);
+        //         if(stats.isDirectory()){
+        //             allAlbums.push(files[i]);
+        //         }
+        //     })
+        // }
         (function iterator(i){
-            if(i == files.length){
-                callback(null,allAlbums);
-                return;
-            }
+            // 循环结束后把收集的数据的数组allAlbums当做callback的参数传递给`router.js`的albums
+            //然后albums再渲染在index.ejs模板中在页面中呈递出来
+            //必须添加 return 语句因为结束循环就要return一下不然整个函数没有返回值就会被一直挂起
+            //next() 方法就不会执行下一条任务,就会显示：next is not a function
+            if(i == files.length) return callback(null,allAlbums);
+               
             fs.stat('./uploads/'+files[i],function(err,stats){
-                if (err) callback("找不到文件啦~"+files[i],null)
-                if(stats.isDirectory()){
+                if (err) callback('找不到文件~'+files[i],null);
+                  
+                if (stats.isDirectory()){
                     allAlbums.push(files[i]);
                 }
                 iterator(i + 1);
-            });
-        })(0);
+            })
+        })(0)
     })
 }
 
-
 exports.getAllImagesByAlbumName = function(albumName,callback){
     fs.readdir('./uploads/'+albumName,function(err,files){
-        if (err) {
-            callback('没找到文件夹',null);
-            return;
-        }
+        if (err) return callback('没有找到文件夹~',null);
+            
         var allImages = [];
         (function iterator(i){
             if(i == files.length){
                 callback(null,allImages);
                 return;
             }
-            fs.stat('./uploads/' + albumName + '/' + files[i],function(err,stats){
-                if (err) callback("找不到文件啦~"+files[i],null)
+            fs.stat('./uploads/'+albumName +'/'+files[i],function(err,stats){
+                if(err) callback('找不到文件~'+files[i],null)
                 if(stats.isFile()){
                     allImages.push(files[i]);
                 }
                 iterator(i + 1);
-            });
-        })(0);
+            })
+        })(0)
     })
 }
